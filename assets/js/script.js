d02e0c7c57f881e7a05d137c75bacd8690cd2571
mosesabrahamroy <mosesroy350@gmail.com>
2024-09-12 17:44:15 +0530

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,16 @@ var symbol;
 
 function button(number) {
     if (symbol) {
+        userinput_2 += number;
+        document.getElementById("input-1").value = userinput_2;
+        console.log(userinput_2, 'input_box_2');
+    } else {
         userinput_1 += number;
         document.getElementById("input-1").value = userinput_1;
         console.log(userinput_1, 'input_box_1');
     }
-    else {
-        userinput_2 += number;
-        document.getElementById("input-1").value = userinput_2;
-        console.log(userinput_2, 'input_box_3');
-    }
 }
+
 function execution(operator) {
     symbol = operator;
     document.getElementById("input-1").value = symbol;
@@ -21,35 +21,32 @@ function execution(operator) {
 }
 
 function result() {
+    let final;
     if (symbol == '/') {
-        let final = parseInt(userinput_1) / parseInt(userinput_2);
-        document.getElementById("input-1").value = final;
-    }
-    else if (symbol == '*') {
-        let final = parseInt(userinput_1) * parseInt(userinput_2);
-        document.getElementById("input-1").value = final;
-    }
-    else if (symbol == '+') {
-        let final = parseInt(userinput_1) + parseInt(userinput_2);
-        document.getElementById("input-1").value = final;
-    }
-    else if (symbol == '-') {
-        let final = parseInt(userinput_1) - parseInt(userinput_2);
-        document.getElementById("input-1").value = final;
-    }
-    else {
+        final = parseInt(userinput_1) / parseInt(userinput_2);
+    } else if (symbol == '*') {
+        final = parseInt(userinput_1) * parseInt(userinput_2);
+    } else if (symbol == '+') {
+        final = parseInt(userinput_1) + parseInt(userinput_2);
+    } else if (symbol == '-') {
+        final = parseInt(userinput_1) - parseInt(userinput_2);
+    } else {
         document.getElementById("input-1").value = "Invalid Operator";
+        return;
     }
+
+    document.getElementById("input-1").value = final;
+
+
+    userinput_1 = final.toString(); 
+    userinput_2 = ''; 
+    symbol = ''; 
 }
 
 function autoclear() {
     userinput_1 = '';
     userinput_2 = '';
     symbol = '';
-    final = '';
-
     document.getElementById("input-1").value = '';
-    document.getElementById("input-2").value = '';
-
 }
 
